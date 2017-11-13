fffec496b7ba3ce7d216557a84a4b15aae3b3046
Raizaen <31708305+Raizaen@users.noreply.github.com>
2017-11-13 12:01:18 +0100

    Tp 13/11/17

---
+++ Exo1.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool triangle(float a, float b, float c) {
+  return (a<=c+b);
+}
+
+bool isocele(float a,float b,float c) {
+  if(b==a || b==c) return 1;
+  return 0;
+}
+
+bool rectangle(float a,float b,float c) {
+  if ((b*b+c*c)==(a*a)) return 1;
+  return 0;
+}
+
+bool equilateral(float a,float b,float c) {
+  if (a==c) return 1;
+  return 0;
+}
+
+bool fplat(float a,float b,float c) {
+  if (a-b-c==0) return 1;
+  return 0;
+}
+
+int main()
+{
+  float x, y, z, s, A, p, a, b, c;
+  
+  cout << "Rentrez les valeurs de vos 3 cotes separees d'espaces: " << endl;
+  cin >> x >> y >> z;
+  
+  p = x+y+z;
+  
+  a = fmax(fmax(x,y),z);
+  c = fmin(fmin(x,y),z);
+  b = p-a-c;
+  
+  if(triangle(a,b,c)){
+    cout << "Les 3 longueurs forment un triangle valide." << endl;
+    s = p / 2;
+    A = sqrt(s*(s-a)*(s-b)*(s-c));
+    
+    cout << "Votre triangle est : ";
+    if(fplat(a,b,c)) {
+      cout << "plat ";
+    } else {
+      if(isocele(a,b,c)) {
+        if(equilateral(a,b,c)) {
+          cout << "equilateral ";
+        } else cout << "isocele ";
+      }
+      if(rectangle(a,b,c)) {
+        cout << "rectangle ";
+      }
+    }
+    cout << endl;
+    cout << "Votre triangle a un perimetre de " << p << " et une aire de " << A << endl;
+  } else cout << "Un triangle ne peut avoir ces 3 longueurs comme cote." << endl;
+  return 0;
+}
---
+++ Exo3.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string askforstring (int linenum) {
+  string usrin = "";
+  cout << "Please write line "<<linenum<<":\n>> ";
+  getline(cin, usrin);
+  return usrin;
+}
+
+string askforstring (string title) {
+  string usrin;
+  cout << "Please write "<<title<<":\n>> ";
+  cin.ignore(); getline(cin,usrin);
+  return usrin;
+}
+
+int askforint (string name) {
+  int usrin;
+  cout << name << " (int):\n>> ";
+  cin >> usrin;
+  return usrin;
+}
+
+int choosemode () {
+  unsigned int mode;
+  cout << "Choose mode:\n 1=copy\n 2=remove\n 3=insert\n 4=pos\n>> ";
+  cin >> mode;
+  if (mode<=4) return mode;
+  return 0;
+}
+
+string getcopy (string instr,int pos,int size) {
+  string outstring = "";
+  int start = pos-1;
+  int end = pos+size-1;
+  for(int i=start;i<end;i++) outstring += instr[i];
+  return outstring;
+}
+
+void copy () {
+  string line1 = askforstring("line to copy from");
+  unsigned int from=-1, size=-1;
+  unsigned linesize = line1.size();
+  while(from>linesize) from = (unsigned int)askforint("From position");
+  while(size>linesize-from+1) size = (unsigned int)askforint("Number of characters");
+  string line2 = getcopy(line1,from,size);
+  cout<<line2<<endl;
+}
+
+string getdel (string instr, int pos, int size) {
+  string outstring = "";
+  int start = pos-1;
+  int end = pos+size-1;
+  for (int i=0; i<instr.size();i++){
+    if (i<start && i<end) outstring += instr[i];
+  }
+  return outstring;
+}
+
+void del () {
+  string line1 = askforstring("line to delete from");
+  unsigned int from=-1, size=-1;
+  unsigned linesize = line1.size();
+  while(from>linesize) from = (unsigned int)askforint("From position");
+  while(size>linesize-from+1) size = (unsigned int)askforint("Number of characters");
+  string line2 = getdel(line1,from,size);
+  cout<<line2<<endl;
+}
+
+string getaddstrings (string instr1, string instr2, int pos) {
+  string outstring = "";
+  if(pos==instr1.size()+1){
+    outstring = instr1+instr2;
+    return outstring;
+  }
+  for(int i=0;i<instr1.size();i++){
+    outstring += instr1[i];
+    if(i==pos-2) outstring +=instr2;
+  }
+  return outstring;
+}
+
+void addstrings () {
+  string line1 = askforstring("line 1 in which we'll add 2");
+  string line2 = askforstring(2);
+  unsigned int from=-1;
+  while(from>line1.size()+1) from = (unsigned int)askforint("Add line 2 to line 1 at");
+  string line3 = getaddstrings(line1,line2,from);
+  cout<<line3<<endl;
+}
+
+int findstringindex (string instr1, string instr2) {
+  int pospos=0;
+  int linstr1 = instr1.size();
+  int linstr2 = instr2.size();
+  if(linstr2<linstr1){
+    return 0;
+  } 
+  int linstr3=linstr2-linstr1+1;
+  for(int i=0; i<linstr3;i++){
+    if(instr1[0]==instr2[i]){
+      pospos=i;
+      for(int j=1;j<linstr1;j++){
+        if(instr1[j]!=instr2[pospos+j]){
+          pospos=0;
+          j=linstr2;
+        }
+        
+      }
+      if(!pospos) return pospos+1;
+    }
+  }
+  return pospos;
+}
+
+void findstring () {
+  string line1 = askforstring("line 1 in which we'll find in 2");
+  string line2 = askforstring(2);
+  int index = findstringindex(line1,line2);
+  cout<<index<<endl;
+}
+
+int main () {
+  int mode=0;
+  while(!mode) mode = choosemode();
+  switch (mode) {
+  case 1:
+    copy();
+    break;
+  case 2:
+    del();
+    break;
+  case 3:
+    addstrings();
+    break;
+  case 4:
+    findstring();
+    break;
+  default:
+    cout << "Only 1 is availiable"<<endl;
+  break;
+  }	
+  
+  return 0;
+}
